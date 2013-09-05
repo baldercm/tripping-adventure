c87065adff44f765eb8cc04b9e5ba23e888758a8
Bálder Carraté <balder.carrate@abacor.es>
2013-09-05 02:32:09 +0200

    JavaScript reverse routes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,12 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready in Java."));
     }
+    
+    public static Result jsRoutes() {
+        return ok( Routes.javascriptRouter("jsRoutes",
+        		routes.javascript.Cursos.listJson(),
+        		routes.javascript.Cursos.createAjax(),
+        		routes.javascript.Cursos.view()));
+    }
   
 }
